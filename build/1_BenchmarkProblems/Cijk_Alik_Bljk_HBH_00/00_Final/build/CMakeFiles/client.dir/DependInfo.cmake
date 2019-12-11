e2003a06ef0f3d16e58a1347fe6f89a2f0e91098
Henry <henry@ixt-hq-59.local.lan>
2019-12-11 08:31:54 +0000

    implement dwordx4 lds_dma_off pad_per_128_bytes

---
+++ build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/Client.cpp" "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/CMakeFiles/client.dir/Client.cpp.o"
+  "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/Kernels.cpp" "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/CMakeFiles/client.dir/Kernels.cpp.o"
+  "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/MathTemplates.cpp" "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/CMakeFiles/client.dir/MathTemplates.cpp.o"
+  "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/SolutionHelper.cpp" "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/CMakeFiles/client.dir/SolutionHelper.cpp.o"
+  "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/Solutions.cpp" "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/CMakeFiles/client.dir/Solutions.cpp.o"
+  "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/source/Tools.cpp" "/home/henry/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/CMakeFiles/client.dir/Tools.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "Tensile_CLIENT_BENCHMARK=1"
+  "Tensile_CLIENT_LIBRARY=0"
+  "Tensile_ENABLE_HALF"
+  "Tensile_RUNTIME_LANGUAGE_HIP=1"
+  "Tensile_RUNTIME_LANGUAGE_OCL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/rocm/include"
+  "/opt/rocm/hcc/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/run.sh
+++ build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/run.sh
@@ -6,7 +6,8 @@ cmake -DTensile_RUNTIME_LANGUAGE=HIP -DTensile_CODE_OBJECT_VERSION=V2 -DTensile_
 echo && echo "################################################################################" && echo "# Building Client" && echo "################################################################################"
 cmake --build . --config Release -- -j 8
 set +e
-#./client --platform-idx 0 --device-idx 0 --init-alpha 1 --init-beta 0 --init-d 0 --init-c 0 --init-a 2 --init-b 2 --c-equal-d 1 --print-valids 0 --print-max 4 --num-benchmarks 1 --num-elements-to-validate -1 --num-enqueues-per-sync 1 --num-syncs-per-benchmark 1 --use-gpu-timer 1 --sleep-percent 200 --benchmark-solutions 0
+#./client --platform-idx 0 --device-idx 0 --init-alpha 1 --init-beta 0 --init-d 0 --init-c 0 --init-a 2 --init-b 2 --c-equal-d 1 --print-valids 0 --print-max 4 --num-benchmarks 1 --num-elements-to-validate 10 --num-enqueues-per-sync 1 --num-syncs-per-benchmark 1 --use-gpu-timer 1 --sleep-percent 200 --benchmark-solutions 0
+#./client --platform-idx 0 --device-idx 0 --init-alpha 1 --init-beta 0 --init-d 0 --init-c 0 --init-a 3 --init-b 3 --c-equal-d 1 --print-valids 0 --print-max 4 --num-benchmarks 1 --num-elements-to-validate 10 --num-enqueues-per-sync 1 --num-syncs-per-benchmark 1 --use-gpu-timer 1 --sleep-percent 200 --benchmark-solutions 0
 ./client --platform-idx 0 --device-idx 0 --init-alpha 1 --init-beta 0 --init-d 0 --init-c 0 --init-a 3 --init-b 3 --c-equal-d 1 --print-valids 0 --print-max 4 --num-benchmarks 20 --num-elements-to-validate 0 --num-enqueues-per-sync 1 --num-syncs-per-benchmark 20 --use-gpu-timer 1 --sleep-percent 0 --benchmark-solutions 0
 ERR1=$?
 #/mnt/Tensile/Tensile/build/0_Build/client/tensile_client --config-file=/mnt/Tensile/Tensile/build/1_BenchmarkProblems/Cijk_Alik_Bljk_HBH_00/00_Final/build/../source/ClientParameters.ini
