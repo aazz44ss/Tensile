1fee26b15b5f638c65d3b9dfc6b0057f69b8b1d4
Henry <hehe790223@gmail.com>
2020-06-05 14:04:18 +0800

    LOCAL: set SUS to 256 if SU=0

--- Tensile/SolutionStructs.py
+++ Tensile/SolutionStructs.py
@@ -2634,6 +2634,7 @@ class Solution:
     state["_staggerStrideShift"] = staggerStrideShift
     if state["StaggerU"] == 0:
       state["StaggerUMapping"] = 0
+      state["StaggerUStride"] = 256
 
     # VectorWidth default handling
     if state["VectorWidth"] < 1:
