35dffb4ed4ad69f342084e25864bbeef530c5a98
Henry Ho <hehe790223@gmail.com>
2020-08-23 14:17:19 +0800

    LOCAL: PrintSolutionRejectionReason for rejected kernel by KernelWriter

--- Tensile/KernelWriter.py
+++ Tensile/KernelWriter.py
@@ -4037,7 +4037,8 @@ for codeObjectFileName in codeObjectFileNames:
       print(exc)
       return (-1, "")
     except RuntimeError as exc:
-      print(exc)
+      if globalParameters["PrintSolutionRejectionReason"]:
+        print(exc)
       return (-2, "")
 
   ##############################################################################
